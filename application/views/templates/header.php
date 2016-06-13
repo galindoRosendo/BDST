18226295edb452ae4c41c34d237c4cd33ca2b750
Rosendo Galindo <rosendo_galindo@hotmail.com>
2016-06-13 06:17:51 -0500

    bootstrap added

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,8 +3,20 @@
   <head>
     <meta charset="utf-8">
     <title>Agenda STI</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   </head>
   <body>
     <html>
 
-                <h1><?php echo $title; ?></h1>
+                <h1>
+                  <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
+                  <?php echo $title; ?>
+                </h1>
